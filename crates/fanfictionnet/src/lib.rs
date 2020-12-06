321c6d167131e9aae480365616b8385fb0bd21dd
François Monniot <francoismonniot@gmail.com>
2020-12-05 16:22:59 -0800

    Create the epub file

--- crates/fanfictionnet/src/lib.rs
+++ crates/fanfictionnet/src/lib.rs
@@ -1,5 +1,8 @@
 use hyper::{body, Client};
-use scraper::{Html, Selector};
+use scraper::{Html, Selector, Node};
+use hyper_tls::HttpsConnector;
+use log::warn;
+use ego_tree::NodeRef;
 
 #[derive(Debug, PartialEq)]
 pub struct StoryId(u32);
@@ -8,7 +11,7 @@ pub fn new_story_id(id: u32) -> StoryId {
     StoryId(id)
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 pub struct ChapterNum(u16);
 
 pub fn new_chapter_number(num: u16) -> ChapterNum {
@@ -17,6 +20,7 @@ pub fn new_chapter_number(num: u16) -> ChapterNum {
 
 const FFN_BASE_URL: &str = "https://www.fanfiction.net";
 
+#[derive(Debug)]
 pub enum Error {
     Http(hyper::Error),
 }
@@ -31,12 +35,35 @@ pub struct Chapter {
     num: ChapterNum,
     title: String,
     story_name: String,
+    author: String,
     content: String,
 }
 
+impl Chapter {
+    pub fn number(&self) -> ChapterNum {
+        self.num
+    }
+
+    pub fn title(&self) -> &String {
+        &self.title
+    }
+
+    pub fn story_title(&self) -> &String {
+        &self.story_name
+    }
+
+    pub fn author(&self) -> &String {
+        &self.author
+    }
+
+    pub fn content(&self) -> &String {
+        &self.content
+    }
+}
+
 pub async fn fetch_story_chapter(sid: StoryId, chapter: ChapterNum) -> Result<Chapter, Error> {
-    // Still inside `async fn main`...
-    let client = Client::new();
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
 
     let url = format!("{}/s/{}/{}", FFN_BASE_URL, sid.0, chapter.0)
         .parse()
@@ -57,7 +84,9 @@ fn parse_chapter(raw_html: String, chapter: ChapterNum) -> Chapter {
     let document = Html::parse_document(&raw_html);
 
     let selector = Selector::parse(".storytext").unwrap();
-    let content = document.select(&selector).next().unwrap().html();
+    let mut buffer = Vec::new();
+    serialize_tree(&mut buffer, &document.select(&selector).next().unwrap());
+    let content = String::from_utf8(buffer).unwrap();
 
     let selector = Selector::parse("#profile_top > b.xcontrast_txt").unwrap();
     let story_name = document.select(&selector).next().unwrap().inner_html();
@@ -65,11 +94,68 @@ fn parse_chapter(raw_html: String, chapter: ChapterNum) -> Chapter {
     let selector = Selector::parse("#chap_select > option[selected]").unwrap();
     let title = document.select(&selector).next().unwrap().inner_html();
 
+    let selector = Selector::parse("#profile_top > a:nth-child(5)").unwrap();
+    let author = document.select(&selector).next().unwrap().inner_html();
+
     Chapter {
         num: chapter,
         title,
         story_name,
+        author,
         content,
+    }
+}
+
+const TAG_LESSER: u8 = '<' as u8;
+const TAG_GREATER: u8 = '>' as u8;
+const TAG_CLOSING: &[u8] = "</".as_bytes();
+const TAG_EQUAL: u8 = '=' as u8;
+const TAG_QUOTE: u8 = '"' as u8;
+const TAG_SPACE: u8 = ' ' as u8;
+
+/// Because we are in need to convert from HTML to xHTML, we can't
+/// use the builtin `.inner_html` function. So instead we build a
+/// very simple tree traverser to serialize the content.
+///
+/// Because the story text on FF.net isn't very deep (usually just two
+/// levels), we use a simple recursion (no real stack overflow risk).
+fn serialize_tree(buffer: &mut Vec<u8>, r: &NodeRef<Node>) {
+    for node in r.children() {
+        match node.value() {
+            Node::Element(el) => {
+                // start tag
+                buffer.push(TAG_LESSER);
+                // tag name
+                buffer.extend_from_slice(el.name().as_bytes());
+                // attributes
+                for (name, value) in el.attrs() {
+                    buffer.push(TAG_SPACE);
+                    buffer.extend_from_slice(name.as_bytes());
+                    buffer.push(TAG_EQUAL);
+                    buffer.push(TAG_QUOTE);
+                    buffer.extend_from_slice(value.as_bytes());
+                    buffer.push(TAG_QUOTE);
+                }
+                // end tag
+                buffer.push(TAG_GREATER);
+
+                // content (recursion)
+                serialize_tree(buffer, &node);
+
+                // start end tag
+                buffer.extend_from_slice(TAG_CLOSING);
+                // tag name
+                buffer.extend_from_slice(el.name().as_bytes());
+                // end end tag
+                buffer.push(TAG_GREATER);
+            }
+            Node::Text(txt) => {
+                let s: &[u8] = (*txt.text).as_bytes();
+
+                buffer.extend_from_slice(s);
+            }
+            _ => warn!("unsupported node type encountered"),
+        }
     }
 }
 
@@ -93,6 +179,6 @@ mod tests {
         assert_eq!(ch.story_name, "The Path of a Jedi");
         assert_eq!(ch.title, "38. Part III, Chapter 1");
         assert_eq!(ch.num, ChapterNum(1));
-        assert_eq!(ch.content.len(), 31465);
+        assert_eq!(ch.content.len(), 31430);
     }
 }
--- crates/gmail-watch/src/main.rs
+++ crates/gmail-watch/src/main.rs
@@ -2,15 +2,24 @@
 
 use std::convert::Infallible;
 
+use epub_builder::EpubBuilder;
+use epub_builder::EpubContent;
+use epub_builder::ReferenceType;
+use epub_builder::ZipLibrary;
+use fanfictionnet::Chapter;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use log::info;
+use log::{error, info};
+use tokio::prelude::*;
 
 struct Notification;
 
+#[derive(Debug)]
 enum Error {
     FFN(fanfictionnet::Error),
     RMCloud(rmcloud::Error),
+    Epub(epub_builder::Error),
+    Io(std::io::Error),
 }
 
 impl From<fanfictionnet::Error> for Error {
@@ -25,16 +34,32 @@ impl From<rmcloud::Error> for Error {
     }
 }
 
+impl From<epub_builder::Error> for Error {
+    fn from(error: epub_builder::Error) -> Self {
+        Error::Epub(error)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
 async fn convert(_notification: Notification) -> Result<(), Error> {
     let _ = fetch_mail_content().await?;
 
     // Will come from the mail content
-    let sid = fanfictionnet::new_story_id(1);
-    let chapter = fanfictionnet::new_chapter_number(1);
+    let sid = fanfictionnet::new_story_id(4985743);
+    let chapter = fanfictionnet::new_chapter_number(38);
 
-    let _chapter = fanfictionnet::fetch_story_chapter(sid, chapter).await?;
+    let chapter = fanfictionnet::fetch_story_chapter(sid, chapter).await?;
 
-    let _epub = make_epub().await?;
+    let epub = make_epub(chapter).await?;
+
+    // Temporarily, until the rmcloud client is correctly working
+    let mut file = tokio::fs::File::create("/Users/francoismonniot/chapter.epub").await?;
+    file.write_all(&epub).await?;
 
     let rm_cloud = rmcloud::make_client()?;
 
@@ -47,8 +72,28 @@ async fn fetch_mail_content() -> Result<(), Error> {
     Ok(())
 }
 
-async fn make_epub() -> Result<(), Error> {
-    Ok(())
+async fn make_epub(chapter: Chapter) -> Result<Vec<u8>, Error> {
+    let mut buffer = Vec::new();
+
+    let content = format!(r#"<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<body>
+{}
+</body>
+</html>"#, chapter.content());
+
+    EpubBuilder::new(ZipLibrary::new()?)?
+        // Set some metadata
+        .metadata("author", chapter.author())?
+        .metadata("title", chapter.story_title())?
+        .add_content(
+            EpubContent::new("chapter_1.xhtml", content.as_bytes())
+                .title(chapter.title())
+                .reftype(ReferenceType::Text),
+        )?
+        .generate(&mut buffer)?;
+
+    Ok(buffer)
 }
 
 /// Handle the interface between the HTTP transport and the business functions
@@ -57,7 +102,10 @@ async fn http_handler(_: Request<Body>) -> Result<Response<Body>, Infallible> {
 
     match convert(notificaton).await {
         Ok(()) => Ok(Response::builder().status(200).body(Body::empty()).unwrap()),
-        Err(_) => Ok(Response::builder().status(500).body(Body::empty()).unwrap()),
+        Err(e) => {
+            error!("Error while handling email: {:?}", e);    
+            Ok(Response::builder().status(500).body(Body::empty()).unwrap())
+        },
     }
 }
 
