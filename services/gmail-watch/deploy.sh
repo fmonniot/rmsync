ac4a45dd2b3a65deeec81c3bb577672806fe012c
François Monniot <francoismonniot@gmail.com>
2020-12-09 21:58:46 -0800

    Change the script to deploy to cloud run instead of just building

--- services/gmail-watch/deploy.sh
+++ services/gmail-watch/deploy.sh
@@ -3,3 +3,7 @@
 docker build -t gcr.io/rmsync/gmail-watch \
              -f ./Dockerfile \
              ../../
+
+docker push gcr.io/rmsync/gmail-watch
+
+gcloud run deploy gmail-watch gcr.io/rmsync/gmail-watch:latest
